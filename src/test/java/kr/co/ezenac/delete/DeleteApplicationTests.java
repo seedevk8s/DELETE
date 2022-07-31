ddf14ac2aba3191c989ab949a639d7448388d42d
seedevk8s <seedevk8s@gmail.com>
2022-07-31 21:43:29 +0900

    first commit

---
+++ src/test/java/kr/co/ezenac/delete/DeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.delete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
